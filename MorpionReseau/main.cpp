267b703177698d85dfa952ac445c1807c3d1262c
Minipoto <msylvestre52600@gmail.com>
2024-02-07 15:25:36 +0100

    Morpion finally work completely

--- MorpionReseau/main.cpp
+++ MorpionReseau/main.cpp
@@ -7,12 +7,11 @@
 
 int main()
 {
-
     GameInstance oGame(600, 600);
 
     sf::RenderWindow window(sf::VideoMode(oGame.x, oGame.y), "MORPION");
 
-    std::vector<GameObject*> list = { nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr };
+    std::vector<GameObject*> list;
     int click = 0;
 
     while (window.isOpen())
@@ -25,29 +24,48 @@ int main()
                 window.close();
             }
 
-            if (event.type == sf::Event::MouseButtonPressed)
+            if (event.type == sf::Event::MouseButtonPressed && click < 9) // Vérifie si le jeu n'est pas terminé
             {
                 if (event.mouseButton.button == sf::Mouse::Left)
                 {
-                    click++;
-                    oGame.switchPlayer();
                     sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-                    list[click - 1] = new GameObject(mousePosition, oGame);
+                    int row = mousePosition.y / (oGame.y / 3);
+                    int col = mousePosition.x / (oGame.x / 3);
+
+                    // Vérifie si la case est vide
+                    if (oGame.getBoard()[row][col] == -1) {
+                        click++;
+                        oGame.switchPlayer();
+                        list.push_back(new GameObject(mousePosition, oGame));
+                        oGame.getBoard()[row][col] = oGame.getPlayerTurn();
+
+                        // Vérifie s'il y a un vainqueur après chaque coup
+                        if (oGame.checkWin(oGame.getPlayerTurn())) {
+                            // Affiche le message de victoire
+                            std::cout << "Joueur " << oGame.getPlayerTurn() << " a gagné !" << std::endl;
+                            window.close();
+                        }
+                    }
                 }
             }
         }
 
+        // Affiche le plateau de jeu
         if (click > 0)
         {
             for (int i = 0; i < click; i++)
             {
-                window.draw(*(list[i]->pShape));
+                if (list[i] != nullptr) {
+                    window.draw(*(list[i]->pShape));
+                }
             }
         }
+
         window.display();
 
-        if (click == 9)
-        {
+        // Vérifie s'il y a égalité
+        if (click == 9) {
+            std::cout << "Match nul !" << std::endl;
             window.close();
         }
     }
