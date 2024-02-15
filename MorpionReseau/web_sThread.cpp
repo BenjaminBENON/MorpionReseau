ccca7ba5032d5fac98d5ccb477b3ed852f8e00fc
ManaIron <117281679+ManaIron@users.noreply.github.com>
2024-02-15 03:50:08 +0100

    Update web_sThread.cpp

--- MorpionReseau/web_sThread.cpp
+++ MorpionReseau/web_sThread.cpp
@@ -1,18 +1,28 @@
 #include "web_sThread.h"
+//#include "httplib.h"
 
+#include "GameInstance.cpp"
 
+//retourne l'Ã©tat actuel du jeu en format JSON.
+extern std::string getGameStateAsJson();
 
 web_sThread::web_sThread()
 {
 }
 
-
 web_sThread::~web_sThread()
 {
 }
 
-
 void web_sThread::OnEnter()
 {
-    //Mettez votre taf ici puis commentez si ca marche pas et supprimez ce commentaire là<-
+  /*  httplib::Server svr;
+
+    svr.Get("/game-state", [&](const httplib::Request&, httplib::Response& res) {
+        std::string gameState = getGameStateAsJson(); 
+        res.set_content(gameState, "application/json");
+        });
+
+    
+    svr.listen("0.0.0.0", 7985);*/
 }
